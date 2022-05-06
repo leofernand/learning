1a88b992df082eb3b903d79ada4d7be04ecfdbfc
leo <leofernandes831@gmail.com>
2022-05-06 10:42:49 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-new SimpleSlide ({
-  slide: "quote", //nome do atributo data-slide="quote"
-  time: 5000 // tempo de transição dos slides
-});
+if (window.SimpleSlide) {
+  new SimpleSlide ({
+    slide: "quote", //nome do atributo data-slide="quote"
+    time: 5000 // tempo de transição dos slides
+  });
 
-new SimpleSlide ({
-  slide: "portfolio", 
-  time: 5000, 
-  nav: true
-})
+  new SimpleSlide ({
+    slide: "portfolio", 
+    time: 5000, 
+    nav: true
+  })
+}
